b4c367a6b207de175abeb541785d03f29277fffd
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-06-25 16:01:18 -0300

    Footer updated

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.mainContainer}></div>
+    </footer>
+  );
+};
+
+export default Footer;
