88b542c25e85043bcae33acc6082c38c0a27d06a
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-06-25 16:01:05 -0300

    Home updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,43 @@
 import styles from "./home.module.css";
 import logo from "/logo/Logo2x.png";
 import { FaExclamationCircle } from "react-icons/fa";
+import Result from "../Result/Result";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <>
-      <img src={logo} alt="logo" className={styles.logo} />
-      <div className={styles.maincontainer}>
-        <div className={styles.textareaContainer}>
-          <textarea
-            name="phrase"
-            placeholder="Ingrese el texto aquí"
-          ></textarea>
-        </div>
-        <div className={styles.actionsContainer}>
-          <p className={styles.clousureContainer}>
-            <FaExclamationCircle className={styles.exclamationIcon} />
-            Solo letras minúsculas y sin acentos
-          </p>
-          <div className={styles.buttonContainer}>
-            <button className={styles.firstButton}>Encriptar</button>
-            <button className={styles.secondButton}>Desencriptar</button>
+      <a href="https://www.aluracursos.com/" target="_blank">
+        <img
+          src={logo}
+          alt="logo"
+          className={styles.logo}
+          title="Alura Cursos"
+        />
+      </a>
+      <div className={styles.container}>
+        <div className={styles.maincontainer}>
+          <div className={styles.textareaContainer}>
+            <textarea
+              name="textarea"
+              placeholder="Ingrese el texto aquí"
+              className={styles.textarea}
+            ></textarea>
+          </div>
+          <div className={styles.actionsContainer}>
+            <p className={styles.clousureContainer}>
+              <FaExclamationCircle className={styles.exclamationIcon} />
+              Solo letras minúsculas y sin acentos
+            </p>
+            <div className={styles.buttonContainer}>
+              <button className={styles.firstButton}>Encriptar</button>
+              <button className={styles.secondButton}>Desencriptar</button>
+            </div>
           </div>
         </div>
+        <Result />
       </div>
+      <Footer />
     </>
   );
 };
