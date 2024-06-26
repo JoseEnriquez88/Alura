4782b84c3c175b5d99b47802a4dbfe696617971e
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-06-26 16:19:49 -0300

    component result + responsive

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,15 +1,28 @@
 import styles from "./result.module.css";
 import muñeco from "/images/Muñeco.png";
 
-const Result = () => {
+const Result = ({ processedText }) => {
   return (
     <div className={styles.mainContainer}>
-      <img src={muñeco} alt="img" className={styles.image} />
-      <div className={styles.textContainer}>
-        <h1>Ningún mensaje fue encontrado</h1>
-        <p>Ingresa el texto que deseas encriptar o desencriptar</p>
-        <button className={styles.copyButton}>Copiar</button>
-      </div>
+      {processedText ? (
+        <div className={styles.textContainer}>
+          <p className={styles.processedText}>{processedText}</p>
+          <button
+            className={styles.copyButton}
+            onClick={() => navigator.clipboard.writeText(processedText)}
+          >
+            Copiar
+          </button>
+        </div>
+      ) : (
+        <>
+          <img src={muñeco} alt="img" className={styles.image} />
+          <div className={styles.textContainer}>
+            <h1>Ningún mensaje fue encontrado</h1>
+            <p className={styles.advice}>Ingresa el texto que deseas encriptar o desencriptar</p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
