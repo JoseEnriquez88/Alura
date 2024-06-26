60fd9bd4fab58c0040d8543ddedff72959e279ba
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-06-25 21:31:43 -0300

    footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,13 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.mainContainer}></div>
+      <div className={styles.textContainer}>
+        Desarrollado por:
+        <a href="https://portfolio-pi-ten-78.vercel.app/" target="_blank">
+          Jose Enriquez
+          <img src="/logo.png" alt="logo" className={styles.logo} />
+        </a>
+      </div>
     </footer>
   );
 };
