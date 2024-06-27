1c398c68ef0d082cebc1ca38b038f35400c6c8a8
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-06-27 08:17:25 -0300

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
     <>
       <Home />
+      <Footer/>
     </>
   );
 };
