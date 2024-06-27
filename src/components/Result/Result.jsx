e1a70c7220dd4ef5098d46b30acfeed3f2d62c9b
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-06-27 08:17:37 -0300

    result component updated

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -3,27 +3,37 @@ import muñeco from "/images/Muñeco.png";
 
 const Result = ({ processedText }) => {
   return (
-    <div className={styles.mainContainer}>
-      {processedText ? (
-        <div className={styles.textContainer}>
-          <p className={styles.processedText}>{processedText}</p>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.mainContainer}>
+          {processedText ? (
+            <div className={styles.textContainer}>
+              <p className={styles.processedText}>{processedText}</p>
+            </div>
+          ) : (
+            <>
+              <img src={muñeco} alt="img" className={styles.image} />
+              <div className={styles.textContainer}>
+                <h1>Ningún mensaje fue encontrado</h1>
+                <p className={styles.advice}>
+                  Ingresa el texto que deseas encriptar o desencriptar
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+        {processedText ? (
           <button
             className={styles.copyButton}
             onClick={() => navigator.clipboard.writeText(processedText)}
           >
             Copiar
           </button>
-        </div>
-      ) : (
-        <>
-          <img src={muñeco} alt="img" className={styles.image} />
-          <div className={styles.textContainer}>
-            <h1>Ningún mensaje fue encontrado</h1>
-            <p className={styles.advice}>Ingresa el texto que deseas encriptar o desencriptar</p>
-          </div>
-        </>
-      )}
-    </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
   );
 };
 
