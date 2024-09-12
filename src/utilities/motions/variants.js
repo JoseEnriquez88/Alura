094a015dca9e901de2ea62aab60873989ad71010
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-12 08:36:05 -0300

    framer motions variants added

---
+++ src/utilities/motions/variants.js
@@ -0,0 +1,38 @@
+export const textAreaVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 2,
+    transition: {
+      duration: 0.8,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+export const firstButtonVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+export const secondButtonVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 2,
+    transition: {
+      duration: 3,
+      staggerChildren: 1,
+    },
+  },
+};
