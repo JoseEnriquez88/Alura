0c38cd61e8aaa3e5d0ddbc45fc1140ead66d6d84
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-12 08:35:38 -0300

    app component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Fragment } from "react";
 import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <>
+    <Fragment>
       <Home />
-      <Footer/>
-    </>
+      <Footer />
+    </Fragment>
   );
 };
 
